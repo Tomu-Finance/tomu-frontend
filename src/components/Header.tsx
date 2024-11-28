ff033aef7922223b046ef633c1a4b396969cb690
Divine Charles <officialkb17@gmail.com>
2024-11-28 11:32:53 +0100

    Build Header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import '../styles/components/Header.scss';
+import Select from './Select';
+import settingsIcon from '../assets/svgs/settings.svg';
+import notificationsIcon from '../assets/svgs/notification.svg';
+import Image from 'next/image';
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <input
+        className="header__input"
+        placeholder="Search for assets, address, chain"
+      />
+      <div className="header__right">
+        <div className="header__right__selectFlex">
+          <Select
+            text="Protocols"
+            iconColor="#00140F"
+            height={45}
+            width={153}
+          />
+          <Select
+            text="All networks"
+            iconColor="#00140F"
+            height={45}
+            width={153}
+          />
+        </div>
+        <Image
+          src={settingsIcon}
+          alt="settings icon"
+          className="header__right__icon settings"
+        />
+        <Image
+          src={notificationsIcon}
+          alt="notification icon"
+          className="header__right__icon"
+        />
+        <button className="header__right__button">Launch app</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
