8750c396d7185a8924f2249af4b2c7079756e7b6
Divine Charles <officialkb17@gmail.com>
2024-11-29 09:32:47 +0100

    Build portfolio ui barebones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/pages/Portfolio.scss';
 
 const HomePage = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className="portfolio">
+      <div className="portfolio__top">
+        <div className="portfolio__top__left"></div>
+        <div className="portfolio__top__right"></div>
+      </div>
+      <div className="portfolio__bottom"></div>
+    </div>
+  );
 };
 
 export default HomePage;
