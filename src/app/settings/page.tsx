1328e3ce7c8a6640e9f0172c06a8c1a94e42af72
Nuel Elekwachi <elekwaeke300@gmail.com>
2025-01-04 00:51:56 +0100

    update passcode feature added

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,40 +1,72 @@
+"use client";
+
 import Image from "next/image";
 import "../../styles/pages/Settings.scss";
 import address from "../../assets/svgs/address.svg";
 import copy from "../../assets/svgs/copy.svg";
 import passcode from "../../assets/svgs/passcode.svg";
 import phraseIcon from "../../assets/svgs/key.svg";
+import { useState } from "react";
 
 export default function Settings() {
+  const [visibleElement, setVisibleElement] = useState(true);
+
+  const handleClick = () => {
+    setVisibleElement(false);
+  };
+
   return (
     <div className="settings-page">
       <h1>Settings</h1>
 
-      <div className="settings-page___address">
-        <div className="settings-page___first">
-          <div className="address-logo">
-            <Image src={address} alt="address-logo" />
-          </div>
-          <h2>Wallet address:</h2>
-          <div className="copy-logo">
-            <p>0x72eBDcd...5ebCa3656F8559F2C073</p>
-            <Image src={copy} alt="copy-logo" />
+      {visibleElement ? (
+        <div className="settings-page___address">
+          <div className="settings-page___first">
+            <div className="address-logo">
+              <Image src={address} alt="address-logo" />
+            </div>
+            <h2>Wallet address:</h2>
+            <div className="copy-logo">
+              <p>0x72eBDcd...5ebCa3656F8559F2C073</p>
+              <Image src={copy} alt="copy-logo" />
+            </div>
+
+            <div className="settings-page___buttons">
+              <div className="copy-logo-two" onClick={handleClick}>
+                <Image src={passcode} alt="passcode-icon" />
+                <p>Update passcode</p>
+              </div>
+              <div className="copy-logo-two">
+                <Image src={phraseIcon} alt="passcode-icon" />
+                <p>Secret phrase and Private Key</p>
+              </div>
+            </div>
           </div>
 
-          <div className="settings-page___buttons">
-            <div className="copy-logo-two">
-              <Image src={passcode} alt="passcode-icon" />
-              <p>Update passcode</p>
-            </div>
-            <div className="copy-logo-two">
-              <Image src={phraseIcon} alt="passcode-icon" />
-              <p>Secret phrase and Private Key</p>
-            </div>
-          </div>
+          <h3>Log out</h3>
         </div>
+      ) : (
+        <div className="settings-page___passcode">
+          <h2>Update Passcode</h2>
 
-        <h3>Log out</h3>
-      </div>
+          <div className="form_contain">
+            <div>
+              <p>Current passcode</p>
+              <input type="password" placeholder="**********" />
+            </div>
+            <div>
+              <p>New passcode</p>
+              <input type="password" placeholder="**********" />
+            </div>
+            <div>
+              <p>Confirm passcode</p>
+              <input type="password" placeholder="**********" />
+            </div>
+          </div>
+
+          <button>Set passcode</button>
+        </div>
+      )}
     </div>
   );
 }
